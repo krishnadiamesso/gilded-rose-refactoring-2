e28c732cc6f1dd710d90ed471f614b0e9f78f13a
Krishna Diamesso <krishnadiamesso@gmail.com>
2024-04-17 06:19:58 -0400

    Refactored

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -4,52 +4,60 @@ class GildedRose
     @items = items
   end
 
-  def update_quality()
+
+  MAXIMUM_QUALITY = 50
+  MINIMUM_QUALITY = 0
+
+  def update_quality
     @items.each do |item|
-      if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
-        if item.quality > 0
-          if item.name != "Sulfuras, Hand of Ragnaros"
-            item.quality = item.quality - 1
-          end
-        end
+
+      case item.name
+      when 'Aged Brie'
+        update_aged_brie(item)
+      when 'Backstage passes to a TAFKAL80ETC concert'
+        update_backstage_passes(item)
       else
-        if item.quality < 50
-          item.quality = item.quality + 1
-          if item.name == "Backstage passes to a TAFKAL80ETC concert"
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-          end
-        end
-      end
-      if item.name != "Sulfuras, Hand of Ragnaros"
-        item.sell_in = item.sell_in - 1
-      end
-      if item.sell_in < 0
-        if item.name != "Aged Brie"
-          if item.name != "Backstage passes to a TAFKAL80ETC concert"
-            if item.quality > 0
-              if item.name != "Sulfuras, Hand of Ragnaros"
-                item.quality = item.quality - 1
-              end
-            end
-          else
-            item.quality = item.quality - item.quality
-          end
-        else
-          if item.quality < 50
-            item.quality = item.quality + 1
-          end
-        end
+        update_normal_item(item)
       end
     end
+  end
+
+  private
+
+  def update_aged_brie(item)
+    quality_increase(item)
+    decrease_sell_in(item)
+    quality_increase(item) if item.sell_in.negative?
+  end
+
+  def update_backstage_passes(item)
+    quality_increase(item)
+    quality_increase(item) if item.sell_in < 11
+    quality_increase(item) if item.sell_in < 6
+    decrease_sell_in(item)
+    quality_zero(item) if item.sell_in.negative?
+  end
+
+  def update_normal_item(item)
+    quality_degrade(item)
+    decrease_sell_in(item)
+    quality_degrade(item) if item.sell_in.negative?
+  end
+
+  def quality_increase(item)
+    item.quality += 1 if item.quality < MAXIMUM_QUALITY
+  end
+
+  def quality_degrade(item)
+    item.quality -= 1 if item.quality.positive? && item.name != 'Sulfuras, Hand of Ragnaros'
+  end
+
+  def decrease_sell_in(item)
+    item.sell_in -= 1 if item.name != 'Sulfuras, Hand of Ragnaros'
+  end
+
+  def quality_zero(item)
+    item.quality = 0
   end
 end
 
